8cc1f31ec56b22bb712c93d539820cbaa42c7c07
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-04-01 10:57:00 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
             let load = document.getElementById('load');
             load.addEventListener('click', () => {
                 console.log('clicked');
+                        
               
               
 
